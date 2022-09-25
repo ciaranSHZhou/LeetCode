9183e653b6bbd853284eb588e97d488b424316ce
ciaranSHZhou <94954162+ciaranSHZhou@users.noreply.github.com>
2022-09-25 10:18:59 -0700

    Time: 74 ms (13.81%), Space: 19.3 MB (57.65%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int zero = 0;
+        
+        for (int i = 0; i < nums.size(); ++i){
+            
+            if (nums[i])
+                swap(nums[zero++], nums[i]);
+        }
+        
+    }
+};
