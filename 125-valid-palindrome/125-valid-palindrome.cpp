1fe21e4502d5b6a57f5cd87ab7c1f2ccb22d2210
ciaranSHZhou <94954162+ciaranSHZhou@users.noreply.github.com>
2022-10-01 23:29:39 -0700

    Time: 11 ms (47.60%), Space: 7.2 MB (90.36%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -10,6 +10,7 @@ public:
         else if (!isalnum(s[end]))
             --end;
         else if (tolower(s[begin]) != tolower(s[end]))
+        //else if ((s[left] + 32 - 'a') %32 != (s[right] + 32 - 'a') % 32) return false;
             return false;
         else{
             ++begin;
