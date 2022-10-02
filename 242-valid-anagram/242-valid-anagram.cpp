e62eeca9e1b9e03d2b82097a62a418ae1484c761
ciaranSHZhou <94954162+ciaranSHZhou@users.noreply.github.com>
2022-10-01 22:49:48 -0700

    Time: 23 ms (33.16%), Space: 7.4 MB (55.96%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -4,14 +4,13 @@ public:
         if(s.size() != t.size())
             return false;
         
-        unordered_map<char, int> m;
-        for (int i = 0; i < s.size(); ++ i){
-            ++m[s[i]];
-            --m[t[i]];
-        }
+        int a[26] = {0};
         
-        for(auto kv: m){
-            if(kv.second != 0)
+        for(int i = 0; i < s.size(); ++i)
+            ++a[s[i] - 'a'];
+        
+        for(int i = 0; i < t.size(); ++i){
+            if (--a[t[i]- 'a'] < 0)
                 return false;
         }
         
