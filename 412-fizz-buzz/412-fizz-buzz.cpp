8351dfe2029194ac2b6596710d0dc1d9e608e9c4
ciaranSHZhou <94954162+ciaranSHZhou@users.noreply.github.com>
2022-10-01 21:57:52 -0700

    Time: 9 ms (24.69%), Space: 7.9 MB (33.92%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> v;
+        
+        for (int i = 1; i < n + 1; ++i){
+            string s = i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : to_string(i));
+            v.push_back(s);
+        }
+        return v;
+    }
+};
