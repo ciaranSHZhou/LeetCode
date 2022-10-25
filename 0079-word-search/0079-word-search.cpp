efb6e2335d38994f9eca679202d21fdcd66729d7
ciaranSHZhou <94954162+ciaranSHZhou@users.noreply.github.com>
2022-10-24 17:54:05 -0700

    Time: 1600 ms (9.87%), Space: 8.2 MB (18.27%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-bool exist(vector<vector<char>>& board, string word) {
-if (board.empty() || board[0].empty()) return false;
-int m = board.size(), n = board[0].size();
-vector<vector<bool>> visited(m, vector<bool>(n));
-for (int i = 0; i < m; ++i) {
-for (int j = 0; j < n; ++j) {
-if (search(board, word, 0, i, j, visited)) return true;
-}
-}
-return false;
-}
-bool search(vector<vector<char>>& board, string word, int idx, int i, int j, vector<vector<bool>>& visited) {
-if (idx == word.size()) return true;
-int m = board.size(), n = board[0].size();
-if (i < 0 || j < 0 || i >= m || j >= n || visited[i][j] || board[i][j] != word[idx]) return false;
-visited[i][j] = true;
-bool res = search(board, word, idx + 1, i - 1, j, visited)
-|| search(board, word, idx + 1, i + 1, j, visited)
-|| search(board, word, idx + 1, i, j - 1, visited)
-|| search(board, word, idx + 1, i, j + 1, visited);
-visited[i][j] = false;
-return res;
-}
+    bool exist(vector<vector<char>>& board, string word) {
+        if (board.empty()|| board[0].empty()) return false;
+        int ROW = board.size();
+        int COL = board[0].size();
+        vector<vector<bool>> visited(ROW, vector<bool>(COL));
+        
+        for (int i = 0; i < ROW; ++ i){
+            for (int j = 0; j < COL; ++j){
+                if (dfs(board, word, 0, i, j, visited)) return true;
+            }
+        }
+        return false;
+    }
+    
+    bool dfs(vector<vector<char>>& board, string word, int word_index, int i, int j, vector<vector<bool>>& visited){
+        if (word_index == word.size()) return true;
+        int ROW = board.size();
+        int COL = board[0].size();
+        
+        if (i < 0 || i > ROW - 1 || j < 0 || j > COL - 1 || visited[i][j] || board[i][j] != word[word_index]) return false;
+        visited[i][j] = true;
+        bool res = dfs(board, word, word_index + 1, i + 1, j, visited) ||
+                   dfs(board, word, word_index + 1, i - 1, j, visited) ||
+                    dfs(board, word, word_index + 1, i, j + 1, visited) ||
+                    dfs(board, word, word_index + 1, i, j - 1, visited);
+        visited[i][j] = false;
+        return res;
+    }
 };
