656d248e2b84db0968cbb488799d3ae99e306416
ciaranSHZhou <94954162+ciaranSHZhou@users.noreply.github.com>
2026-03-25 02:50:55 -0500

    Time: 3 ms (78.25%), Space: 11.2 MB (98.48%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int bestLen = INT_MAX;
+        int bestStart = 0;
+
+        int left = 0;
+        int right = 0;
+
+        int missing = t.size();
+        vector<int> need(128, 0);
+
+        for (auto c : t){
+            ++need[c];
+        }
+
+        while (right < s.size()){
+            char r = s[right];
+            if (need[r] > 0){
+                --missing;
+            }
+            --need[r];
+            ++right;
+
+
+            while (missing == 0){
+                int currLen = right - left;
+                if (currLen < bestLen){
+                    bestLen = currLen;
+                    bestStart = left;
+                }
+
+                char l = s[left];
+                ++need[l];
+                if (need[l] > 0){
+                    ++missing;
+                }
+                ++left;
+            }
+
+
+        }
+
+        if (bestLen == INT_MAX) return "";
+        return s.substr(bestStart, bestLen);
+
+    }
+};
